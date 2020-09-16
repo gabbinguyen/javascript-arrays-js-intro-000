ae98baa844b69b547fba8a1f31757d4aeaea2354
magical-builder-5600 <gabbinguyen@gmail.com>
2020-09-16 23:36:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,8 +22,3 @@ function destructivelyAddElementToEndOfArray (array, element) {
   array.push(element);
   return array;
 }
-
-function accessElementInArray (array, index) {
-  array[];
-  return array;
-}
